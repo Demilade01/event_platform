12931d91d0e439079f9f6fec72c81c89e463b2e8
Deelad <demiladeakinyemi2004@gmail.com>
2024-01-04 07:58:21 -0800

    implement stripe

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from 'next/cache'
 
 import { connectToDatabase } from '@/lib/database'
 import User from '@/lib/database/models/user.model'
+import Order from '@/lib/database/models/order.model'
 import Event from '@/lib/database/models/event.model'
 import { handleError } from '@/lib/utils'
 
@@ -33,6 +34,18 @@ export async function getUserById(userId: string) {
   }
 }
 
+export async function updateUser(clerkId: string, user: UpdateUserParams) {
+  try {
+    await connectToDatabase()
+
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, { new: true })
+
+    if (!updatedUser) throw new Error('User update failed')
+    return JSON.parse(JSON.stringify(updatedUser))
+  } catch (error) {
+    handleError(error)
+  }
+}
 
 export async function deleteUser(clerkId: string) {
   try {
@@ -54,7 +67,7 @@ export async function deleteUser(clerkId: string) {
       ),
 
       // Update the 'orders' collection to remove references to the user
-      // Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
+      Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
     ])
 
     // Delete user
